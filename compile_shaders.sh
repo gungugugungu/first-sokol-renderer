ca03130604ec872096c607a11b6aaf9c071afa8c
Gungu <ddoggames7@gmail.com>
2025-06-28 17:40:46 +0200

    that's automatic shader compilation for you mister

---
+++ compile_shaders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+find . -type f -name "*.glsl" | while read -r shader_file; do
+    # shader directory
+    dir_path=$(dirname "$shader_file")
+    # base name of the shader file
+    base_name=$(basename "$shader_file")
+    # output file name
+    output_file="$dir_path/${base_name}.h"
+
+    echo "Compiling: $shader_file -> $output_file"
+    ./sokol-shdc --input "$shader_file" --output "$output_file" --slang glsl430
+done
--- main.cpp
+++ main.cpp
@@ -25,7 +25,8 @@ static void init(void) {
         // positions            // colors
         0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,  // bottom left
-        0.0f,  0.5f, 0.0f,      0.0f, 0.0f, 1.0f    // top
+        0.5f,  0.5f, 0.0f,      0.0f, 0.0f, 1.0f,    // top right
+        -0.5f,  0.5f, 0.0f,      1.0f, 0.0f, 1.0f    // top left
     };
 
     // vertex buffer
